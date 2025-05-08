a664e5c51cc5b9d661abfd8a2d412068087b7e20
spiffytech <git@spiffy.tech>
2025-05-07 20:38:45 -0400

    feat: introduced e2e tests for the web UI, starting with the new authentication flow (#716)

--- web/src/overflow-menu.js
+++ web/src/overflow-menu.js
@@ -447,6 +447,7 @@ export class OverflowMenu extends EventTarget {
       section.append(divider);
       const logoutText = this.action_manager.text('logout');
       actions.logout = make_button(section, logoutText, 'logout', on_click);
+      actions.logout.dataset.testid = 'logout';
     }
 
     // HELP
